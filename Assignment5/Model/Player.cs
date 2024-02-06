744859315efafac161b5341413ad03f134ef9f6c
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-06 15:08:01 +0530

    Add project files.

---
+++ Assignment5/Model/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment5.Model
+{
+    public partial class Player
+    {
+        public int PlayerId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string? LastName { get; set; }
+        public int JerseyNumber { get; set; }
+        public string Position { get; set; } = null!;
+        public string Team { get; set; } = null!;
+    }
+}
